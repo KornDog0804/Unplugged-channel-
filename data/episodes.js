0fb858a4939b51705c85d227e790804e216c2383
KornDog0804 <titans.rule1215@gmail.com>
2026-01-21 09:03:34 -0600

    Update episodes.js

--- data/episodes.js
+++ data/episodes.js
@@ -15,7 +15,7 @@ window.EPISODES = [
       {
         title: "Full Session Stream",
         // FULL SHOW (not the 6 min song)
-        url: "https://www.youtube.com/watch?v=EGcDLX-vtwU"
+        url: "https://youtu.be/pOTkCgkxqyg"
       }
     ]
   },
@@ -56,12 +56,12 @@ window.EPISODES = [
     year: 2024,
     mode: "queue",
     tracks: [
-      { title: "Track 1", url: "https://youtu.be/zxvZO7MzYzU?si=keOOKrw0ybhgae2T" },
-      { title: "Track 2", url: "https://youtu.be/HasZm8N83cE?si=xPO0UVPuNPhf3fmy" },
-      { title: "Track 3", url: "https://youtu.be/2lcJUfE2LsQ?si=mQZVRyYzITg7Nbic" },
-      { title: "Track 4", url: "https://youtu.be/fT9SJV8KCE8?si=tgBbj0EWr9VKe5SW" },
-      { title: "Track 5", url: "https://youtu.be/laiLOhKO9yU?si=BqOQY6o-E3m_r6BQ" },
-      { title: "Track 6", url: "https://youtu.be/7WVGNUVsDFw?si=7OfgIf9zPXuBiPmT" }
+      { title: "Track 1", url: "https://youtu.be/zxvZO7MzYzU" },
+      { title: "Track 2", url: "https://youtu.be/HasZm8N83cE" },
+      { title: "Track 3", url: "https://youtu.be/2lcJUfE2LsQ" },
+      { title: "Track 4", url: "https://youtu.be/fT9SJV8KCE8" },
+      { title: "Track 5", url: "https://youtu.be/laiLOhKO9yU" },
+      { title: "Track 6", url: "https://youtu.be/7WVGNUVsDFw" }
     ]
   },
 
@@ -71,7 +71,7 @@ window.EPISODES = [
     year: 2023,
     mode: "fullshow",
     tracks: [
-      { title: "Full Session Stream", url: "https://youtu.be/EGB3xoa7Cus?si=o41j-c7WmVmjiXAh" }
+      { title: "Full Session Stream", url: "https://youtu.be/EGB3xoa7Cus" }
     ]
   },
 
@@ -81,10 +81,10 @@ window.EPISODES = [
     year: 2020,
     mode: "queue",
     tracks: [
-      { title: "With This Knife", url: "https://youtu.be/Hl_qZX32LiY?si=8npuZ_WDr6E1afdn" },
-      { title: "Silhouettes", url: "https://youtu.be/mk78hkKzXMA?si=vcQqvVXCKg-mcAFT" },
-      { title: "Bottom Of A Bottle", url: "https://youtu.be/cxIoBp8xHwQ?si=9mQm3SWlilDnRiby" },
-      { title: "Wonderwall", url: "https://youtu.be/C08X2DbSPfs?si=LIe7hhVYI7bBHIKX" }
+      { title: "With This Knife", url: "https://youtu.be/Hl_qZX32LiY" },
+      { title: "Silhouettes", url: "https://youtu.be/mk78hkKzXMA" },
+      { title: "Bottom Of A Bottle", url: "https://youtu.be/cxIoBp8xHwQ" },
+      { title: "Wonderwall", url: "https://youtu.be/C08X2DbSPfs" }
     ]
   },
 
@@ -94,8 +94,8 @@ window.EPISODES = [
     year: 2020,
     mode: "queue",
     tracks: [
-      { title: "Body Bag", url: "https://youtu.be/jkY3QORBHeE?si=mk2-smKWYfIKEquH" },
-      { title: "Hangfire", url: "https://youtu.be/pqWTvbLTV0Q?si=GPByTp3t8Qmdp6iA" }
+      { title: "Body Bag", url: "https://youtu.be/jkY3QORBHeE" },
+      { title: "Hangfire", url: "https://youtu.be/pqWTvbLTV0Q" }
     ]
   }
 ];
