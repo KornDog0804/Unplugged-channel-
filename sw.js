6e7f97fe97ec4c869477deb1643a64b20ac6e3cd
KornDog0804 <titans.rule1215@gmail.com>
2026-02-05 14:21:01 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,10 +7,12 @@ const CORE_ASSETS = [
   "./styles.css",
   "./app.js",
   "./data/episodes.js",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
@@ -18,25 +20,17 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-// Activate
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+      Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key))))
     )
   );
   self.clients.claim();
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
