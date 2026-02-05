4346ee68172e5d49a643d206f999465574dc533f
KornDog0804 <titans.rule1215@gmail.com>
2026-02-05 07:38:27 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "joeys-acoustic-corner-v1";
+
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./sessions.html",
+  "./styles.css",
+  "./app.js",
+  "./data/episodes.js",
+  "./manifest.webmanifest"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
